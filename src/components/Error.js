7fa84ad5b96d23c911f3423055d9eb93d71ded43
harshraj152003 <harsh912823@gmail.com>
2025-09-22 23:46:36 +0530

    updated error.js , error.vss

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,34 +1,34 @@
 import React from 'react';
-// import '../styles/Error.css'; // Don't forget to create this CSS file!
-
-// const Error = () => {
-//     return (
-//         <div className="error-container">
-//             <div className="error-content">
-//                 <img
-//                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPm949IcUeOWGe2c-AbxptOouPaPuN6r14Aw&s"
-//                     alt="Confused cat looking at a computer screen"
-//                     className="error-cat-image"
-//                 />
-//                 <h1 className="error-title">Meow! Aww, Snap!</h1>
-//                 <p className="error-message">Looks like something went wrong... Even our expert cat engineers are stumped!</p>
-//                 <p className="error-sub-message">Don't worry, we're on it! Try heading back home or refreshing the page.</p>
-//                 <a href="/" className="home-button">Take Me Home!</a>
-//             </div>
-//         </div>
-//     );
-// }
-
 import { useRouteError } from 'react-router-dom';
+import '../styles/Error.css';
 
-const Error = ()=> {
-    const err = useRouteError()
+const Error = () => {
+    const err = useRouteError();
+
     return (
-        <div className='error'>
-            <h1>{err.status + " " + err.statusText}</h1>
-            <h3>{err.data}</h3>
+        <div className="full-screen-container">
+            <div className="error-container">
+                <div className="error-content">
+                    <img
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPm949IcUeOWGe2c-AbxptOouPaPuN6r14Aw&s"
+                        alt="Confused cat looking at a computer screen"
+                        className="error-cat-image"
+                    />
+                    <h1 className="error-title">Meow! Aww, Snap!</h1>
+                    <p className="error-message">
+                        Looks like something went wrong... Even our expert cat engineers are stumped!
+                    </p>
+                    <h3 className="error-status">
+                        {err.status} {err.statusText}
+                    </h3>
+                    <p className="error-sub-message">
+                        {err.data || "Don't worry, we're on it! Try heading back home or refreshing the page."}
+                    </p>
+                    <a href="/" className="home-button">Take Me Home!</a>
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default Error;
